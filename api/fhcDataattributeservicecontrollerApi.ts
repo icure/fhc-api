dfbcb75134787702fb92b76eb81aa4ec49b01e66
Sam Jocqué <sam.jocque@medispring.be>
2022-03-02 11:52:02 +0100

    MS-5894: include daas controller in FHC-API

--- api/fhcDataattributeservicecontrollerApi.ts
+++ api/fhcDataattributeservicecontrollerApi.ts
@@ -28,9 +28,16 @@ import * as models from "../model/models"
 export class fhcDataattributeservicecontrollerApi {
   host: string
   headers: Array<XHR.Header>
-  constructor(host: string, headers: any) {
+  fetchImpl?: (input: RequestInfo, init?: RequestInit) => Promise<Response>
+
+  constructor(
+    host: string,
+    headers: any,
+    fetchImpl?: (input: RequestInfo, init?: RequestInit) => Promise<Response>
+  ) {
     this.host = host
     this.headers = Object.keys(headers).map(k => new XHR.Header(k, headers[k]))
+    this.fetchImpl = fetchImpl
   }
 
   setHeaders(h: Array<XHR.Header>) {
@@ -74,7 +81,7 @@ export class fhcDataattributeservicecontrollerApi {
     headers = headers.concat(new XHR.Header("X-FHC-keystoreId", xFHCKeystoreId))
     headers = headers.concat(new XHR.Header("X-FHC-tokenId", xFHCTokenId))
     headers = headers.concat(new XHR.Header("X-FHC-passPhrase", xFHCPassPhrase))
-    return XHR.sendCommand("GET", _url, headers, _body)
+    return XHR.sendCommand("GET", _url, headers, _body, this.fetchImpl)
       .then(doc => new models.DaasResponse(doc.body as JSON))
       .catch(err => this.handleError(err))
   }
